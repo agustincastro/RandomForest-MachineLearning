90985a6f5ba5dfd7b2f926e1131c36e7e22423de
Agustin Castro <agustin.castro.91@gmail.com>
2016-05-25 23:53:12 -0300

    Added naive bayes implementation

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+        name='Machine-learning-analysis',
+        version='',
+        packages=['tests', 'resources', 'learning_logic', 'console_interface'],
+        url='',
+        license='',
+        author='Agustin',
+        author_email='',
+        description=''
+)
+
+
